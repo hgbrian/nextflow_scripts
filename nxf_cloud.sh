71ae5923ee521331a68064dd3406c1edabed5087
Brian Naughton <briannaughton@gmail.com>
2016-12-30 08:14:51 -0800

    minor improvements upon starting a VPC from scratch

--- nxf_cloud.sh
+++ nxf_cloud.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
+
+# 1. Create an AWS VPC:  ./nxf_cloud.sh create_vpc
+# 2. 
+
 # ----------------------------------------------------------------------------------------
 # Globals (should match nextflow.config)
+# All these environment variables will need to be set to run `nextflow cloud`
 #
 username="${NXF_username}"
 github_url="${NXF_github_url}"
 
-required_env_vars="NXF_test
-NXF_username
+# ----------------------------------------------------------------------------------------
+#
+env_vars_file="env_vars.${NXF_username}.export"
+
+required_env_vars="NXF_username
 NXF_github_url
 NXF_AWS_subnet_id
 NXF_AWS_efs_id
-NXF_AWS_accessKey
-NXF_AWS_secretKey
+NXF_AWS_efs_mnt
 NXF_AWS_container_id
-NXF_AWS_efs_mnt"
+NXF_AWS_accessKey
+NXF_AWS_secretKey"
 
 available_fns="describe_vpc
 create_vpc
@@ -22,7 +30,10 @@ create_nextflow_cluster
 shutdown_nextflow_cluster"
 
 # ----------------------------------------------------------------------------------------
-# setup
+# initial setup
+# Get information on which environment vars are set.
+# Get information on whether a cluster exists/
+# Get external IP.
 #
 initial_setup() {
     is_env_set=""
@@ -32,13 +43,14 @@ initial_setup() {
     do
         # http://unix.stackexchange.com/questions/251893/get-environment-variable-by-variable-name
         if [ "${!env_var}" ]; then 
-            is_env_set="$is_env_set,$env_var"
+            is_env_set="$is_env_set $env_var"
         else
-            is_env_not="$is_env_not,$env_var"
+            echo "yes"
+            is_env_not="$is_env_not $env_var"
         fi
     done
-    if [ is_env_set == "" ]; then is_env_set="None"; else is_env_set="${is_env_set:1}"; fi
-    if [ is_env_not == "" ]; then is_env_not="None"; else is_env_not="${is_env_not:1}"; fi
+    if [ "${is_env_set}" == "" ]; then is_env_set="[None]"; else is_env_set="${is_env_set:1}"; fi
+    if [ "${is_env_not}" == "" ]; then is_env_not="[None]"; else is_env_not="${is_env_not:1}"; fi
 
     #
     # use username_sha where the raw username is not appropriate
@@ -74,12 +86,13 @@ initial_setup() {
     echo "================================"
     echo "| current config               |"
     echo "================================"
-    echo "username:     $username"
-    echo "external_ip:  $external_ip"
-    echo "vpc:          $vpc_id"
-    echo "cluster:     " $clusterinfo # remove newlines in output by removing quotes
-    echo "is_env_set:   $is_env_set"
-    echo "is_env_not:   $is_env_not"
+    echo "username:       $username"
+    echo "external_ip:    $external_ip"
+    echo "vpc:            $vpc_id"
+    echo "cluster:       " $clusterinfo # remove newlines in output by removing quotes
+    echo "envs_are_set:   $is_env_set"
+    echo "envs_not_set:   $is_env_not"
+    echo "functions:     " $available_fns
 }
 
 
@@ -97,7 +110,7 @@ create_vpc() {
     #
     vpcinfo=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --profile $username)
     vpc_id=$(echo "$vpcinfo" |cut -f7)
-    echo "vpc_id:       $vpc_id"
+    echo "vpc_id:       ${vpc_id}"
 
     aws ec2 modify-vpc-attribute --vpc-id $vpc_id --enable-dns-hostnames --profile $username
     
@@ -106,8 +119,7 @@ create_vpc() {
     #
     subnetinfo=$(aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.0.0/24 --profile $username)
     subnet_id=$(echo "$subnetinfo" |grep $vpc_id |cut -f9)
-    echo "subnet_id:    $subnet_id    [setting env NXF_AWS_subnet_id]"
-    export NXF_AWS_subnet_id="${subnet_id}"
+    echo "subnet_id:    ${subnet_id}    [setting env NXF_AWS_subnet_id]"
     
     aws ec2 modify-subnet-attribute --subnet-id $subnet_id --map-public-ip-on-launch --profile $username
 
@@ -116,7 +128,7 @@ create_vpc() {
     # Creating a security group and attaching it did not work but would be better
     #
     sg_id=$(aws ec2 describe-security-groups --output text --profile nextflowuser |grep $vpc_id |cut -f3)
-    echo "sg_id:        $sg_id"
+    echo "sg_id:        ${sg_id}"
          
     aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 22 --cidr $external_ip/32 --profile $username
     aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 22 --source-group "$sg_id" --profile $username
@@ -125,14 +137,14 @@ create_vpc() {
     # Internet gateway
     #
     igw_id=$(aws ec2 create-internet-gateway --profile $username |cut -f 2)
-    echo "igw_id:       $igw_id"
+    echo "igw_id:       ${igw_id}"
     aws ec2 attach-internet-gateway --internet-gateway-id $igw_id --vpc-id $vpc_id --profile $username
 
     #
     # Route tables and route
     #
     rtb_id=$(aws ec2 describe-route-tables --output text --profile $username |grep $vpc_id |cut -f2)
-    echo "rtb_id:       $rtb_id"
+    echo "rtb_id:       ${rtb_id}"
 
     isroute=$(aws ec2 create-route --route-table-id $rtb_id --gateway-id $igw_id --destination-cidr-block 0.0.0.0/0 --profile $username)
     if [ "$isroute" != "True" ]; then 
@@ -143,41 +155,40 @@ create_vpc() {
 
 describe_vpc() {
     echo "================================"
-    echo "| describe vpc                 |"
+    echo "| describe vpc / set up env    |"
     echo "================================"
 
     vpcinfo=$(aws ec2 describe-vpcs --output text --profile $username)
     vpc_id=$(echo "$vpcinfo" | cut -f7)
-    echo "vpc:          $vpcinfo"
-    echo "vpc_id:       $vpc_id"
-    
-    echo "---------"
     
     if [ "$vpc_id" != "" ]; then
+        echo "vpc_id:       ${vpc_id}"
+
         subnetinfo=$(aws ec2 describe-subnets --output text --profile $username)
-        subnet_id=$(echo "$subnetinfo" | grep $vpc_id | cut -f9)
-        echo "subnet:       $subnetinfo"
-        echo "subnet_id:    $subnet_id"
-    
-        echo "---------"
+        subnet_id=$(echo "${subnetinfo}" | grep $vpc_id | cut -f9)
+        echo "subnet_id:    ${subnet_id}"
+        echo "export NXF_AWS_subnet_id="${subnet_id}"" |tee >"${env_vars_file}"
+
         sginfo=$(aws ec2 describe-security-groups --output text --profile $username | grep $vpc_id)
         sg_id=$(echo "$sginfo" | cut -f3)
-        echo "sg_id:        $sg_id"
+        echo "sg_id:        ${sg_id}"
 
         eni_id=$(aws ec2 describe-network-interfaces --output text --profile $username | grep $vpc_id | cut -f5)
-        echo "eni_id:       $eni_id"
+        echo "eni_id:       ${eni_id:=[None]}"
 
         rtb_id=$(aws ec2 describe-route-tables --output text --profile $username | grep $vpc_id | cut -f2)
-        echo "rtb_id:       $rtb_id"
+        echo "rtb_id:       ${rtb_id:=[None]}"
     
         igw_id=$(aws ec2 describe-internet-gateways --profile $username |grep -B1 $vpc_id |head -1 | cut -f2)
-        echo "igw_id:       $igw_id"
+        echo "igw_id:       ${igw_id:=[None]}"
 
         efs_id=$(aws efs describe-file-systems --profile $username | grep "^FILESYSTEMS" | head -1 | cut -f4)
-        echo "efs_id:       $efs_id"
+        echo "efs_id:       ${efs_id:=[None]}"
+        echo "export NXF_AWS_efs_id="${efs_id}"" |tee >"${env_vars_file}"
 
         ecr_url=$(aws ecr describe-repositories --profile $username | grep "^REPOSITORIES" | head -1 | cut -f6)
-        echo "ecr_url:      $ecr_url"
+        echo "ecr_url:      ${ecr_url:=[None]}"
+        echo "export NXF_AWS_container_id="${ecr_url}"" |tee >"${env_vars_file}"
         
     else
         echo "No vpc exists; exiting"
@@ -187,8 +198,9 @@ describe_vpc() {
 
 
 # ----------------------------------------------------------------------------------------
-# shut down VPC, delete all traces
-# variables come from describe_vpc
+# Shut down VPC, attempt to delete all traces from AWS
+# This can go wrong if the EFS is till mounted.
+# Variables in here come are initialized during describe_vpc
 #
 shutdown_vpc() {
     echo "================================"
@@ -205,27 +217,35 @@ shutdown_vpc() {
     aws ec2 delete-vpc --vpc-id $vpc_id --profile $username
 }
 
-
+# ----------------------------------------------------------------------------------------
+# Set up an EFS on AWS and mount it.
+# EFS initially uses no space, and grows as you add files to it.
+#
 setup_efs() {
     echo "================================"
     echo "| setup efs                    |"
     echo "================================"
 
-    efsinfo=$(aws efs describe-file-systems --profile $username | grep "^FILESYSTEMS")
+    efsinfo=$(aws efs describe-file-systems --profile "${username}" | grep "${username_sha}" | grep "^FILESYSTEMS")
 
-    if [ "$efsinfo" == '' ]; then
-        echo "Creating EFS using id $username_sha"
-        efsinfo = $(aws efs create-file-system --creation-token $username_sha --profile $username)
-        efs_id=$(echo "$efsinfo" | cut -f3)
+    if [ "${efsinfo}" == '' ]; then
+        echo "No EFS exits. Creating EFS using id $username_sha:"
+        efsinfo=$(aws efs create-file-system --creation-token $username_sha --profile $username)
+        efs_id=$(echo "${efsinfo}" | cut -f3)
     else
-        efs_id=$(echo "$efsinfo" | cut -f4)
+        echo "EFS exists."
+        efs_id=$(echo "${efsinfo}" | cut -f4)
     fi
-    echo "efs_id:       $efs_id"
+    echo "efs_id:         ${efs_id}"
 
-    # I may not need to do this (cut -f4 to get fsmt-id)
+    echo "[WARNING] Not mounting fs"
+    # I may not need to do this? (cut -f4 to get fsmt-id)
     #aws efs create-mount-target --file-system-id fs-6af50da3 --subnet-id subnet-9a4462ec --profile nextflowuser
 }
 
+# ----------------------------------------------------------------------------------------
+# Set up a container repo on AWS, if it doesn't already exist
+#
 setup_ecr() {
     echo "================================"
     echo "| setup ecr                    |"
@@ -235,21 +255,21 @@ setup_ecr() {
 
     ecrinfo=$(aws ecr describe-repositories --profile $username | grep "^REPOSITORIES")
     #REPOSITORIES    1482947163.0    319133706199    arn:aws:ecr:eu-west-1:319133706199:repository/nextflowuser_repo nextflowuser_repo       319133706199.dkr.ecr.eu-west-1.amazonaws.com/nextflowuser_repo
-    if [ "$ecrinfo" == '' ]; then
-        echo "No ECR ($reponame) found, creating one"
+    if [ "${ecrinfo}" == '' ]; then
+        echo "No ECR (${reponame}) found, creating one"
         ecrinfo=$(aws ecr create-repository --repository-name $reponame --profile $username)
-        ecr_url=$(echo "$ecrinfo" | cut -f6)
+        ecr_url=$(echo "${ecrinfo}" | cut -f6)
     else
-        if [ $(echo "$ecrinfo" | cut -f5) != "$reponame" ]; then
-            echo "Error in repo name? $(echo $ecrinfo | cut -f5) should be $reponame"
+        if [ $(echo "${ecrinfo}" | cut -f5) != "${reponame}" ]; then
+            echo "Error in repo name? $(echo ${ecrinfo} | cut -f5) should be ${reponame}"
         fi
-        ecr_url=$(echo "$ecrinfo" | cut -f6)
+        ecr_url=$(echo "${ecrinfo}" | cut -f6)
     fi
-    echo "ecr_url:     $ecr_url"
+    echo "ecr_url:       ${ecr_url}"
     
     # This command provides a password for docker authentication
     docker_login_cmd=$(aws ecr get-login --profile $username)
-    echo "Logging in to ECR using command: $(echo $docker_login_cmd | perl -pe 's/-p (.+?) (.+)/-p pwd $2/g')"
+    echo "Logging in to ECR using command: $(echo ${docker_login_cmd} | perl -pe 's/-p (.+?) (.+)/-p pwd $2/g')"
     $docker_login_cmd
 }
 
@@ -321,25 +341,30 @@ ENDSSH
 initial_setup
 
 if [ $# -eq 0 ]; then
-    echo "No arguments supplied:" $available_fns
+    printf "\n[No arguments supplied]"
     exit
 fi
 
 arg=$1
 if [ $arg == "create_vpc" ]; then
     create_vpc
+    describe_vpc
 elif [ $arg == "setup_efs" ]; then
     setup_efs
+    describe_vpc
 elif [ $arg == "setup_ecr" ]; then
     setup_ecr
+    describe_vpc
 elif [ $arg == "shutdown_vpc" ]; then
     shutdown_nextflow_cluster
-    describe_vpc
     shutdown_vpc
+    describe_vpc
 elif [ $arg == "create_nextflow_cluster" ]; then
     create_nextflow_cluster
+    describe_vpc
 elif [ $arg == "shutdown_nextflow_cluster" ]; then
     shutdown_nextflow_cluster
+    describe_vpc
 elif [ $arg == "describe_vpc" ]; then
     describe_vpc
 fi
